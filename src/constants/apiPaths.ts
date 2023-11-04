90695e374ce7b0f0b1cfac70b7c9f5a984e30b59
Sándor Schwarz <sandor.schwarz89@gmail.com>
2023-11-04 23:06:50 +0100

    update api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: 'https://97jrjiuin0.execute-api.eu-west-1.amazonaws.com',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://7b38526d3a.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://97jrjiuin0.execute-api.eu-west-1.amazonaws.com',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com',
+  order: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com',
+  cart: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
