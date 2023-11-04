8735420daa503da360011e3dc2df874cbc9d177c
Sándor Schwarz <sandor.schwarz89@gmail.com>
2023-11-05 00:06:30 +0100

    update api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com',
   order: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://7b38526d3a.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com',
   cart: 'https://a8f4ltzpd3.execute-api.eu-west-1.amazonaws.com/dev',
 };
